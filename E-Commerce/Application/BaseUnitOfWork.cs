7bbb6aef3819c38de52a88f5873f688bf6936332
Salma Negm <negmsalma23@gmail.com>
2022-08-09 22:25:57 +0200

    base unitof work

---
+++ E-Commerce/Application/BaseUnitOfWork.cs
@@ -0,0 +1,55 @@
+﻿namespace ECommerce.Application
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+    using System.Threading.Tasks;
+
+    public class BaseUnitOfWork<T> : IBaseUnitOfWork<T> where T : BaseEntity
+    {
+        protected ApplicationDbContext _context;
+
+        private readonly IBaseRepository<T> _baseRepository;
+        public BaseUnitOfWork(IBaseRepository<T> baseRepository)
+        {
+            _baseRepository = baseRepository;
+            _context = baseRepository.Context;
+        }
+
+        public async Task<T> CreateAsync(T entity)
+        {
+          return await _baseRepository.AddAsync(entity);
+          await  _context.SaveChangesAsync();
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+          await   _baseRepository.EditAsync(entity);
+          await _context.SaveChangesAsync();
+            return entity;
+           
+        }
+
+        public async Task<T> DeleteAsync(Guid id)
+        {
+           T result= await _baseRepository.DeleteAsync(id);
+            await _context.SaveChangesAsync();
+            return result;
+
+        }
+
+        public async Task<List<T>> ReadAsync()
+        {
+            List<T> values = await _baseRepository.GetAllAsync();
+           await _context.SaveChangesAsync();
+            return values;
+        }
+
+        public async Task<T> ReadByIdAsync(Guid id)
+        {
+           T result =   await _baseRepository.GetByIdAsync(id);
+          await   _context.SaveChangesAsync();
+            return result;
+        }
+    }
+}
---
+++ E-Commerce/Application/IBaseUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿namespace ECommerce.Application
+{
+    public interface IBaseUnitOfWork<T> where T :class
+    {
+       Task<T> CreateAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(Guid id);
+        Task<List<T>> ReadAsync();
+        Task<T> ReadByIdAsync(Guid id);
+     
+    }
+}
